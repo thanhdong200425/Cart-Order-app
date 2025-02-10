94b2dab92ed88cf4d15de277bf7fb75d131a7619
thanhdong200425 <dongnt.22ite@vku.udn.vn>
2025-02-10 11:50:36 +0700

    Refactor SignIn component and styles; remove unused dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,10 @@
-import './assets/css/App.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Container from "@mui/material/Container";
-import SignIn from './components/authentication/SignIn';
-
-
+import "./assets/css/App.css";
+import SignIn from "./components/authentication/SignIn";
 
 export default function App() {
-    return <Container maxWidth="sm">
-      <SignIn />
-    </Container>
+    return (
+        <div>
+            <SignIn />
+        </div>
+    );
 }
-
--- client/src/components/authentication/SignIn.jsx
+++ client/src/components/authentication/SignIn.jsx
@@ -1,8 +1,10 @@
-import { Container,TextField } from "@mui/material";
-
 export default function SignIn() {
-    return <Container maxWidth="sm">
+    return (
+        <div>
             <h4>Sign in page</h4>
-            <TextField name="email" type="email" label="Email"/>
-    </Container>
+            <form>
+                <input />
+            </form>
+        </div>
+    );
 }
